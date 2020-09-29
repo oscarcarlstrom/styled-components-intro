69c5d30e3bb4a726a48a5a6df3862cd649919dcb
Oscar <oscar.carlstrom@bouvet.no>
2020-09-29 15:04:50 +0200

    Removed test file

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders h1', () => {
-  const { getByText } = render(<App />);
-  const h1 = getByText(/Example - JS vs TS/i);
-  expect(h1).toBeInTheDocument();
-});
