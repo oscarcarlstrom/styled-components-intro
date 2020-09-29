93a15b09f07223180dfe7cf599ab7aa0534b9280
Carl Oscar Fredrik Karlström <oscarl.carlstrom@statnett.no>
2020-09-29 09:53:49 +0200

    Fixed ts theme

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -1,7 +1,1 @@
-import globalTheme from '../styles/theme';
-import cloneDeep from 'lodash.clonedeep';
-
-const theme = cloneDeep(globalTheme);
-theme.colors.primary = "#0377c7";
-
-export default theme
+export { default } from '../styles/theme';
